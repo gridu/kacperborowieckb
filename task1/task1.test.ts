f78d033a5668b12b2764b6646e4c56ce01b15ff4
Kacper Borowiec <kborowiec@griddynamics.com>
2024-11-18 10:10:17 +0100

    refactor: change test in task 1 to use .resolves

--- task1/task1.test.ts
+++ task1/task1.test.ts
@@ -14,55 +14,43 @@ describe('task1', () => {
     mockFetchIsUserNameAvailable.mockClear();
   });
 
-  it('returns false if name has length less than 3 symbols', async () => {
-    const result = await validateUserName('Jo');
-
-    expect(result).toBe(false);
+  it('returns false if name has length less than 3 symbols', () => {
+    expect(validateUserName('Jo')).resolves.toBe(false);
   });
 
-  it('returns false if name do not contains only alphanumeric symbols', async () => {
-    const result = await validateUserName('John/');
-
-    expect(result).toBe(false);
+  it('returns false if name do not contains only alphanumeric symbols', () => {
+    expect(validateUserName('John/')).resolves.toBe(false);
   });
 
-  it('returns false if name contains spaces', async () => {
-    const result = await validateUserName('John ');
-
-    expect(result).toBe(false);
+  it('returns false if name contains spaces', () => {
+    expect(validateUserName('John ')).resolves.toBe(false);
   });
 
-  it('returns false if name starts with a number', async () => {
-    const result = await validateUserName('2John');
-
-    expect(result).toBe(false);
+  it('returns false if name starts with a number', () => {
+    expect(validateUserName('2John')).resolves.toBe(false);
   });
 
-  it('calls fetchIsUserAvailable method to check if name is unique', async () => {
-    await validateUserName(validName);
+  it('calls fetchIsUserAvailable method to check if name is unique', () => {
+    validateUserName(validName);
 
     expect(mockFetchIsUserNameAvailable).toHaveBeenCalledWith(validName);
   });
 
-  it('do not make any request if name is not valid', async () => {
-    await validateUserName('Jo');
+  it('do not make any request if name is not valid', () => {
+    validateUserName('Jo');
 
     expect(mockFetchIsUserNameAvailable).not.toHaveBeenCalled();
   });
 
-  it('return true with valid name when fetchIsUserAvailable succeed', async () => {
-    const result = await validateUserName(validName);
-
-    expect(result).toBe(true);
+  it('return true with valid name when fetchIsUserAvailable succeed', () => {
+    expect(validateUserName(validName)).resolves.toBe(true);
   });
 
-  it('returns false if fetchIsUserAvailable throws', async () => {
+  it('returns false if fetchIsUserAvailable throws', () => {
     mockFetchIsUserNameAvailable.mockImplementation(() => {
       throw new Error();
     });
 
-    const result = await validateUserName(validName);
-
-    expect(result).toBe(false);
+    expect(validateUserName(validName)).resolves.toBe(false);
   });
 });
