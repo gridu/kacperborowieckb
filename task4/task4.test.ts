51e47e2e0faa06e0f656b4060e3777c4b4332cca
Kacper Borowiec <kborowiec@griddynamics.com>
2024-11-19 19:21:44 +0100

    test: tests for task 4

--- task4/task4.test.ts
+++ task4/task4.test.ts
@@ -1,5 +1,20 @@
 import { validateMathExpression } from './index';
 
 describe('task 5', () => {
-  // the place for your parametrized tests
+  it.each([
+    '1+1,23',
+    '(2*(2-2))',
+    '(4)(2 / 2^2.24)',
+    '(2 * 2) / (1 + 4)',
+    '(((4.2) * 2 + 1) + 23) / -1',
+  ])('recognized %p as valid math expression', (mathExpression) => {
+    expect(validateMathExpression(mathExpression)).toBe(true);
+  });
+
+  it.each(['1x', '2x2', '()', '2,,23', '2..2', '(2)22/', 'some text'])(
+    'recognized %p as invalid math expression',
+    (mathExpression) => {
+      expect(validateMathExpression(mathExpression)).toBe(false);
+    }
+  );
 });
