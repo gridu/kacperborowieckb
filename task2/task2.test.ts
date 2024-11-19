a652a4cf408fe651aa43f994cd28d82d643d7dc9
Kacper Borowiec <kborowiec@griddynamics.com>
2024-11-19 13:52:27 +0100

    refactor: add additional describe for grouping tests

--- task2/task2.test.ts
+++ task2/task2.test.ts
@@ -10,85 +10,89 @@ const createValidateErrorMessage = (message: string) => {
 };
 
 describe('QuantityValidator', () => {
-  it('constructor accepts valid valued for threshold and packageSize', () => {
-    expect(new QuantityValidator(20, 10)).not.toThrow();
+  describe('constructor', () => {
+    it('accepts valid valued for threshold and packageSize', () => {
+      expect(new QuantityValidator(20, 10)).not.toThrow();
+    });
+
+    it('throws an error when threshold is a negative value', () => {
+      expect(new QuantityValidator(-2, 10)).toThrow(
+        new Error('threshold cannot be a negative value')
+      );
+    });
+
+    it('accepts threshold with 0 value', () => {
+      expect(new QuantityValidator(0, 10)).not.toThrow();
+    });
+
+    it('throws an error when packageSize is negative', () => {
+      expect(new QuantityValidator(10, -2)).toThrow(
+        new Error('packageSize should be a positive value')
+      );
+    });
+
+    it('throws an error when packageSize is zero', () => {
+      expect(new QuantityValidator(10, 0)).toThrow(
+        new Error('packageSize should be a positive value')
+      );
+    });
   });
 
-  it('constructor throws an error when threshold is a negative value', () => {
-    expect(new QuantityValidator(-2, 10)).toThrow(
-      new Error('threshold cannot be a negative value')
-    );
-  });
+  describe('validate()', () => {
+    it('should be defined', () => {
+      const validator = new QuantityValidator(10, 5);
 
-  it('constructor accepts threshold with 0 value', () => {
-    expect(new QuantityValidator(0, 10)).not.toThrow();
-  });
+      expect(validator.validate).toBeDefined();
+    });
 
-  it('constructor throws an error when packageSize is negative', () => {
-    expect(new QuantityValidator(10, -2)).toThrow(
-      new Error('packageSize should be a positive value')
-    );
-  });
+    it('returns success validation when quantity is less than threshold ', () => {
+      const validator = new QuantityValidator(10, 5);
 
-  it('constructor throws an error when packageSize is zero', () => {
-    expect(new QuantityValidator(10, 0)).toThrow(
-      new Error('packageSize should be a positive value')
-    );
-  });
+      expect(validator.validate(8)).toEqual(successMessage);
+    });
 
-  it('should have validate() function', () => {
-    const validator = new QuantityValidator(10, 5);
+    it('returns success validation when quantity is more than threshold and divisible by packageSize', () => {
+      const validator = new QuantityValidator(10, 5);
 
-    expect(validator.validate).toBeDefined();
-  });
+      expect(validator.validate(15)).toEqual(successMessage);
+    });
 
-  it('validate() returns success validation when quantity is less than threshold ', () => {
-    const validator = new QuantityValidator(10, 5);
+    it('returns success validation when quantity is equal threshold and divisible by packageSize', () => {
+      const validator = new QuantityValidator(10, 5);
 
-    expect(validator.validate(8)).toEqual(successMessage);
-  });
+      expect(validator.validate(10)).toEqual(successMessage);
+    });
 
-  it('validate() returns success validation when quantity is more than threshold and divisible by packageSize', () => {
-    const validator = new QuantityValidator(10, 5);
+    it('returns failed validation when quantity is more than threshold and not divisible by packageSize', () => {
+      const validator = new QuantityValidator(10, 5);
 
-    expect(validator.validate(15)).toEqual(successMessage);
-  });
+      expect(validator.validate(12)).toEqual(
+        createValidateErrorMessage('Quantity should be divisible by 5')
+      );
+    });
 
-  it('validate() returns success validation when quantity is equal threshold and divisible by packageSize', () => {
-    const validator = new QuantityValidator(10, 5);
+    it('returns failed validation when quantity is equal threshold and not divisible by packageSize', () => {
+      const validator = new QuantityValidator(10, 6);
 
-    expect(validator.validate(10)).toEqual(successMessage);
-  });
+      expect(validator.validate(10)).toEqual(
+        createValidateErrorMessage('Quantity should be divisible by 6')
+      );
+    });
 
-  it('validate() returns failed validation when quantity is more than threshold and not divisible by packageSize', () => {
-    const validator = new QuantityValidator(10, 5);
+    it('returns failed validation when quantity is less than 0', () => {
+      const validator = new QuantityValidator(10, 6);
 
-    expect(validator.validate(12)).toEqual(
-      createValidateErrorMessage('Quantity should be divisible by 5')
-    );
-  });
+      expect(validator.validate(-2)).toEqual(
+        createValidateErrorMessage('Quantity should be a positive value')
+      );
+    });
 
-  it('validate() returns failed validation when quantity is equal threshold and not divisible by packageSize', () => {
-    const validator = new QuantityValidator(10, 6);
+    it('returns failed validation when quantity is equal 0', () => {
+      const validator = new QuantityValidator(10, 6);
 
-    expect(validator.validate(10)).toEqual(
-      createValidateErrorMessage('Quantity should be divisible by 6')
-    );
-  });
-
-  it('validate() returns failed validation when quantity is less than 0', () => {
-    const validator = new QuantityValidator(10, 6);
-
-    expect(validator.validate(-2)).toEqual(
-      createValidateErrorMessage('Quantity should be a positive value')
-    );
-  });
-
-  it('validate() returns failed validation when quantity is equal 0', () => {
-    const validator = new QuantityValidator(10, 6);
-
-    expect(validator.validate(0)).toEqual(
-      createValidateErrorMessage('Quantity should be a positive value')
-    );
+      expect(validator.validate(0)).toEqual(
+        createValidateErrorMessage('Quantity should be a positive value')
+      );
+    });
   });
 });
