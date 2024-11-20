86e8c07f082a8a97a8150b084190de1db323e2a6
Kacper Borowiec <kborowiec@griddynamics.com>
2024-11-20 12:02:03 +0100

    test: tests for task 5

--- task5/task5.test.ts
+++ task5/task5.test.ts
@@ -1,19 +1,41 @@
 import { GlobalCounter } from './index';
 
 describe('GlobalCounter', () => {
+  let GlobalCounterIsolate: typeof GlobalCounter;
+
+  beforeEach(() => {
+    jest.isolateModules(() => {
+      GlobalCounterIsolate = require('./index').GlobalCounter;
+    });
+  });
+
   it('has initial value "0"', () => {
-    // write isolated test
+    expect(GlobalCounterIsolate.getValue()).toBe(0);
   });
 
   it('increments counter', () => {
-    // write another isolated test
+    GlobalCounterIsolate.increment();
+
+    expect(GlobalCounterIsolate.getValue()).toBe(1);
   });
 
   it('decrements value', () => {
-    // your tests could be here
+    GlobalCounterIsolate.decrement();
+
+    expect(GlobalCounterIsolate.getValue()).toBe(-1);
   });
 
   it('multiplies counter', () => {
-    // you won't believe :)
+    GlobalCounterIsolate.multiply(4);
+
+    expect(GlobalCounterIsolate.getValue()).toBe(0);
+  });
+
+  it('multiplies counter after increment action', () => {
+    GlobalCounterIsolate.increment();
+    GlobalCounterIsolate.increment();
+    GlobalCounterIsolate.multiply(4);
+
+    expect(GlobalCounterIsolate.getValue()).toBe(8);
   });
 });
